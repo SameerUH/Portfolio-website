69b16f0a6ada0f2f1569bfefa0ea4a57bbf53cd6
SameerUH <sameerulhaq2005@gmail.com>
2025-06-17 13:15:56 +0100

    A small attempt of implementing good practice CSS to my PHP files, still figuring out how I'm going to reimplement my entire stylesheets

--- pages/index.php
+++ pages/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <!--
 TODO LIST:
-- First add buttons and make divs of the initial design.
-- Decide on a colour scheme to use.
+xxx First add buttons and make divs of the initial design.
+xxx Decide on a colour scheme to use.
 xxxx Implement the 3D aspect of three.js.
-- Decide on the design of the buttons.
-- Possibly add an animated background, (black background with white something????).
+xxx Decide on the design of the buttons.
+xxx Possibly add an animated background, (black background with white something????).
+--- Research modern/good practice in web development and implement it in this code.
+--- Find a good way to use CSS rather than having multiple files???
 -->
 <html lang="en">
 <head>
@@ -27,13 +29,13 @@ xxxx Implement the 3D aspect of three.js.
     </div><br>
 
     <div id="aboutme">
-        <p><strong><u>About Me:</u></strong><br>
+        <p><span class="underline bold text-large">About Me:</span><br>
             I'm Sameer Ul Haq, a second-year Cyber Security student at Northumbria University with a passion for tech, security, and making a meaningful impact. I'm CCNA certified and regularly develop my skills through TryHackMe and Hack The Box.<br><br>I enjoy programming in Python, C, and JavaScript — building everything from small games to full-stack apps — and aim to grow into a well-rounded cybersecurity professional.<br><br>
 
-            <strong><u>Interests:</u></strong><br>
+            <span class="underline bold text-large">Interests:</span><br>
             I'm most excited when working on projects that challenge me creatively and technically. I've developed applications like Pong, GratiThink (a web/mobile app that promotes student wellbeing), and interactive portfolio websites using modern tools like React and Three.js. I also enjoy problem-solving, collaborative teamwork, and continuously learning how different programming languages can be applied to help people in practical ways.<br><br>
 
-            <strong><u>Motiviations:</u></strong><br>
+            <span class="underline bold text-large">Motiviations:</span><br>
             I believe technology should protect and empower. Cybersecurity is evolving and is key to safeguarding people and data and I'm inspired by how tech can protect people from various attackers.
         </p>
         <div class = "image-swap-container">
