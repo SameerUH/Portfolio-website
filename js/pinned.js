84418985dee8a6356b437a8c4684afb9ba5924b3
SameerUH <sameerulhaq2005@gmail.com>
2025-08-03 13:33:25 +0100

    Added some minor changes and imports. --- Imported OrbitControls making it so the user can move the camera and zoom in and out (might remove it). --- Imported Edgelines and and geometry libraries to add edges to the cubes. --- Made the textures on the cubes more clearer using setPixelRatio. --- Add textures to most sides of the cubes and made it so they start spinning.

--- js/pinned.js
+++ js/pinned.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import {OrbitControls} from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import {EdgesGeometry, LineSegments, LineBasicMaterial} from 'three';
+
 
 const container = document.getElementById("new-pinned");
 
@@ -8,7 +11,10 @@ camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setPixelRatio(window.devicePixelRatio); //Makes the image way cleaner and better quality.
 container.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const projects = [
     {name: 'Pong', image: '../assets/thumbnails/pong-thumbnail.png'},
@@ -17,23 +23,32 @@ const projects = [
 ];
 
 const loader = new THREE.TextureLoader();
+const cubes = [];
 projects.forEach((project, index) => {
     loader.load(project.image, (texture) => {
-        const geometry = new THREE.BoxGeometry(1, 1, 0.2)
-        const material = new THREE.MeshStandardMaterial({map: texture});
-        const cube = new THREE.Mesh(geometry, material);
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const materials = [new THREE.MeshStandardMaterial({map: texture}), new THREE.MeshStandardMaterial({map: texture}), new THREE.MeshBasicMaterial({color:0x0000ff}), new THREE.MeshBasicMaterial({color:0x0000ff}), new THREE.MeshStandardMaterial({map: texture}), new THREE.MeshStandardMaterial({map: texture})];
+        const cube = new THREE.Mesh(geometry, materials);
+        const edges = new EdgesGeometry(cube.geometry);
+        const edgeLines = new LineSegments(edges, new LineBasicMaterial({color:0x000000}))
+        cube.add(edgeLines);
         cube.position.x = index * 2 - (projects.length - 1);
         cube.userData = {name: project.name}
-    scene.add(cube);
+        cubes.push(cube)
+        scene.add(cube);
     });
 });
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
+const light = new THREE.DirectionalLight(0xffffff, 3);
 light.position.set(5,5,10).normalize();
 scene.add(light);
 
 function animate() {
     requestAnimationFrame(animate);
+    cubes.forEach(cube => {
+        cube.rotation.y -= 0.01;
+    });
+    controls.update();
     renderer.render(scene, camera)
 }
 animate();
