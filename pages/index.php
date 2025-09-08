273a0401c8c4e7f42065ae4454dffad03f5207ad
SameerUH <sameerhaq2005@gmail.com>
2025-09-08 12:49:42 +0100

    Pretty sure I've finished the timeline section. --- Added all the relevant contents for 2024 and 2025. --- Added an @media section which make the timeline responsive depending on your screen resolution. --- May add a 2023 section to talk about previous projects and education. --- Unsure about the layout of the timeline as it just looks like alot of lists.

--- pages/index.php
+++ pages/index.php
@@ -41,8 +41,8 @@ xxx Change images to cubes that you can rotate.
                 I believe technology should protect and empower. Cybersecurity is evolving and is key to safeguarding people and data and I'm inspired by how tech can protect people from various attackers.
             </p>
             <div class = "image-swap-container">
-                <img class="base-image" src="../assets/thumbnails/profilepicture.jpg" alt="originalimg">
-                <img class="hover-image" src="../assets/thumbnails/archery.jpg" alt="hoverimg">
+                <img class="base-image" src="../assets/thumbnails/archery.jpg" alt="originalimg">
+                <img class="hover-image" src="../assets/thumbnails/profilepicture.jpg" alt="hoverimg">
             </div>
         </div>
     </div>
@@ -59,17 +59,17 @@ xxx Change images to cubes that you can rotate.
             <div class="year">2024</div>
             <div class="text">
                 <div class="modules"><strong>UNIVERSITY MODULES (1st year):</strong>
-                <br>Computers and Society (56%): Research paper on PEGI laws and regulations.
+                <br>Computers and Society (56%): Research paper on PEGI laws and regulations going over why children play videogames over their age and what companies do about it.
                 <br>Data Fundamentals (40%): Written report on data security concepts and development of graphs based on big data using Python (Pandas, Matplotlib).
                 <br>Programming (97%): Developed a productivity app using Python (Flask) with HTML and CSS including features such as to-do lists, calendars, creation and deletion of notes and user creation and authentication.
                 <br>Introduction to Networks and Cyber Security (57%): Learned basic device configuration (switches, routers and hosts), IP addressing schemes and networking protocols/practices.
                 <br>Computing Fundamentals (67%): Created a Caesar Cipher with x86 assembly language whilst learning set notation and logic gates.
-                <br>Computational Thinking (77%): Designed wireframes for our own and existing applications.</div><br>
+                <br>Computational Thinking (77%): Designed wireframes for our own and existing applications to learn the process of planning an app before programming it.</div><br>
                 <div class="projcerts"><strong>PROJECTS/CERTIFICATES:</strong>
-                    <br>CCNAv7: Introductions to Networks.
-                    <br>Productivity App - Python.</div><br>
+                    <br>CCNAv7: Introductions to Networks (Merit) - Gained foundational knowledge in networking concepts, IP addressing, and router/switch configuration through hands-on labs.
+                    <br>Productivity App (Python) - University assignment listed above.</div><br>
                 <div class="extra"><strong>EXTRA-CURRICULAR:</strong>
-                    <br>Attended various networking event learning more about the industry.
+                    <br>Attended various networking events learning more about the industry.
                     <br>Member of Cyber Clinic to learn more about Penetration testing.
                     <br>Attended CTFs hosted by the university.
                     <br>Learning various different areas such as bash scripting and JavaScript to improve my portfolio.
@@ -87,13 +87,14 @@ xxx Change images to cubes that you can rotate.
                 <br>Digital Forensics Incident Response (80%): Investigated and analyzed a simulated criminal case using tools such as Autopsy, DB browser, FTK Registry Viewer and Volatility learning various terms such as windows artefacts, imaging cases, etc.
                 <br>Mobile & Web App Development (72%): Developed a website and mobile app using HTML, CSS, PHP, JavaScript and React with features such as filtered reviews, components, pages, routes, error pages, etc.</div><br>
             <div class="projcerts"><strong>PROJECTS/CERTIFICATES:</strong>
-                <br>CCNA: Switching, Routing and Wireless Essentials
-                <br>GratiThink.
-                <br>Pong.
-                <br>Portfolio website.</div><br>
+                <br>CCNA: Switching, Routing and Wireless Essentials (Merit) - Developed further understanding of VLAN configurations, inter-VLAN routing, OSPF and wireless networks through practical lab excercises.
+                <br>GratiThink - Developed a critical thinking, gratitude journalling app and website for students as a university assignment including features such as entry creations and deletions, and statistics tracker for progressive results.
+                <br>Pong (Python, Pygame) - Developed the classic arcade game outside of university using OOP with abstraction, polymorphism and classes to implement features such as computer controlled enemy, screens (start, settings, etc), score tracker.
+                <br>Portfolio website (PHP, JavaScript, CSS) - The website you are currently looking at was developed as a personal project to showcase my skills, experience and creativity through projects and timelines.</div><br>
             <div class="extra"><strong>EXTRA-CURRICULAR:</strong>
                 <br>Configured a Kali Linux Environment on a Raspberry Pi for experimenting with penetration testing outside of university.
-                <br>Started paths on TryHackMe and started practicing my penetration testing on HackTheBox machines.
+                <br>Started paths on TryHackMe (Jr Penetration Tester, SOC Analyst level 1) and started practicing my penetration testing on HackTheBox machines.
+                <br>Learnt Three.js with JavaScript for my portfolio website which taught me how to create 3D objects, interactions and lighting effects.
         </div>
         </div>
         </div>
