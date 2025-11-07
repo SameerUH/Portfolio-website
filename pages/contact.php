9692f7b0f6496a41de7da4c3eb655f9dcece5451
SameerUH <sameerulhaq2005@gmail.com>
2025-11-07 21:02:46 +0000

    Fixed the social section and started the form. --- Added favicon to the contacts page. --- Added clickable icons to the social section which redirects them to the corresponding page. --- Started creating a form using formspree but still need to make the form look nice.

--- pages/contact.php
+++ pages/contact.php
@@ -9,6 +9,7 @@ TODO LIST:
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/contact.css">
+    <link rel="icon" href="../favicon.png" type="image/png">
     <title>SameerUH | Contact</title>
 </head>
 <body>
@@ -23,17 +24,26 @@ TODO LIST:
 
     <div id="socialsection">
         <div class="row">
-            <a href=""><img src="../assets/linked_in_logo.png" alt="LINKEDIN LOGO"></a>
-            <a href=""><img src="../assets/github.png" alt="GITHUB LOGO"></a>
-        </div>
-        <div class="row">
+            <a href="https://www.linkedin.com/in/sameer-ul-haq-6861b923b"><img src="../assets/thumbnails/socialsicons/linkedin1.jpg" alt="LINKEDIN LOGO"></a>
             <a href=""><iframe src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=3019828"></iframe></a>
-            <a href="https://www.hackthebox.eu/home/users/profile/1925576"><img src="https://www.hackthebox.com/badge/image/1925576"></a>
+            <a href="https://github.com/SameerUH"><img src="../assets/thumbnails/socialsicons/github.jpg" alt="GITHUB LOGO"></a>
         </div>
     </div><br>
 
     <div id="form">
-        <p><strong>EMAIL SECTION.</strong></p>
+        <!-- modify this form HTML and place wherever you want your form -->
+        <form action="https://formspree.io/f/mdkynobk" method="POST">
+        <label>
+            Your email:
+            <input type="email" name="email">
+        </label>
+        <label>
+            Your message:
+            <textarea name="message"></textarea>
+        </label>
+        <!-- your other form fields go here -->
+        <button type="submit">Send</button>
+        </form>
     </div><br>
 
     <?php
