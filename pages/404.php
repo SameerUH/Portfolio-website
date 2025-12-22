54d6d0cae342e8e22c526fc7fcffd3317d46dc65
SameerUH <sameerulhaq2005@gmail.com>
2025-12-22 16:40:40 +0000

    Added an error page using code from gratithink website project :), went pretty well and smoothly but had some issues with centering haha

---
+++ pages/404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/PORTFOLIO/styles/404.css">
+    <link rel="icon" href="/PORTFOLIO/favicon.png" type="image/png">
+    <title>Nice try...</title>
+</head>
+<body>
+    <?php
+    include $_SERVER['DOCUMENT_ROOT'] . "/PORTFOLIO/includes/background_and_import.php";
+    include $_SERVER['DOCUMENT_ROOT'] . "/PORTFOLIO/includes/navbar_fragment.php";
+    ?>
+
+    <h1>This is not the page you are looking for...</h1>
+
+    <a href="/PORTFOLIO/index" id="return">RETURN HOME</a><br><br><br>
+
+    <?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/PORTFOLIO/includes/footer_fragment.php';
+    ?>
+</body>
+</html>
